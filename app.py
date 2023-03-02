bc33abb1119dc4f4c9f6d751b4b4b25e0c01c52b
Suman Kumar <sumanku0176@gmail.com>
2023-03-02 08:04:46 +0530

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template,request,jsonify
 
 app = Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def home_page():
+    return render_template('index.html')
 
 @app.route("/")
 def hello_world():
